a7406d8d790c1152242d6c742a71e00682c4868c
SafariLeader <sollichn@ethz.ch>
2017-06-14 10:21:22 +0000

    modified model_input to work with rgb and depth

--- sample_code/model_input.py
+++ sample_code/model_input.py
@@ -1,27 +1,51 @@
 import tensorflow as tf
 
-def preprocessing_op(image_op, config):
+def preprocessing_op(frame, config):
     """
     Creates preprocessing operations that are going to be applied on a single frame.
-
+    
     TODO: Customize for your needs.
     You can do any preprocessing (masking, normalization/scaling of inputs, augmentation, etc.) by using tensorflow operations.
-    Built-in image operations: https://www.tensorflow.org/api_docs/python/tf/image
+    Built-in image operations: https://www.tensorflow.org/api_docs/python/tf/image 
     """
     with tf.name_scope("preprocessing"):
+        rgb = frame[0]
+        seg = frame[1]
+        dep = frame[2]
+        
         # Reshape serialized image.
-        image_op = tf.reshape(image_op, (config['img_height'],
-                                         config['img_width'],
-                                         config['img_num_channels'])
-                          )
+        rgb = tf.reshape(rgb, (config['img_height'], 
+                               config['img_width'], 
+                               config['img_num_channels'])
+                        )
+        
+        seg = tf.reshape(seg, (config['img_height'], 
+                               config['img_width'], 
+                               config['img_num_channels'])
+                        )
+        
+        dep = tf.reshape(dep, (config['img_height'], 
+                               config['img_width'], 
+                               1)
+                        )
+        
         # Convert from RGB to grayscale.
-        image_op = tf.image.rgb_to_grayscale(image_op)
-
+        rgb = tf.image.rgb_to_grayscale(rgb)
+        seg = tf.image.rgb_to_grayscale(seg)
+        dep = tf.image.rgb_to_grayscale(dep)
+        
         # Integer to float.
-        image_op = tf.to_float(image_op)
+        rgb = tf.to_float(rgb)
+        dep = tf.to_float(dep)
+        
+        seg = tf.greater(seg, 150)
+        
+        rgb = tf.where(seg, rgb, tf.zeros_like(rgb))
+        dep = tf.where(seg, dep, tf.zeros_like(rgb))
+        
         # Crop
         #image_op = tf.image.resize_image_with_crop_or_pad(image_op, 60, 60)
-
+        
         # Resize operation requires 4D tensors (i.e., batch of images).
         # Reshape the image so that it looks like a batch of one sample: [1,60,60,1]
         #image_op = tf.expand_dims(image_op, 0)
@@ -29,22 +53,27 @@ def preprocessing_op(image_op, config):
         #image_op = tf.image.resize_bilinear(image_op, np.asarray([32,32]))
         # Reshape the image: [32,32,1]
         #image_op = tf.squeeze(image_op, 0)
-
-        # Normalize (zero-mean unit-variance) the image locally, i.e., by using statistics of the
+        
+        # Normalize (zero-mean unit-variance) the image locally, i.e., by using statistics of the 
         # image not the whole data or sequence.
-        image_op = tf.image.per_image_standardization(image_op)
-
+        
+        rgb = tf.image.per_image_standardization(rgb)
+        dep = tf.image.per_image_standardization(dep)
+        
+        rgb = tf.squeeze(rgb)
+        dep = tf.squeeze(dep)
+        
         # Flatten image
         #image_op = tf.reshape(image_op, [-1])
-
-        return image_op
+    
+        return tf.stack([rgb, dep], axis=-1)
 
 def read_and_decode_sequence(filename_queue, config):
     # Create a TFRecordReader.
     readerOptions = tf.python_io.TFRecordOptions(compression_type=tf.python_io.TFRecordCompressionType.GZIP)
     reader = tf.TFRecordReader(options=readerOptions)
     _, serialized_example = reader.read(filename_queue)
-
+    
     # Read one sequence sample.
     # The training and validation files contains the following fields:
     # - label: label of the sequence which take values between 1 and 20.
@@ -59,7 +88,7 @@ def read_and_decode_sequence(filename_queue, config):
     with tf.name_scope("TFRecordDecoding"):
         context_encoded, sequence_encoded = tf.parse_single_sequence_example(
                 serialized_example,
-                # "label" and "lenght" are encoded as context features.
+                # "label" and "lenght" are encoded as context features. 
                 context_features={
                     "label": tf.FixedLenFeature([], dtype=tf.int64),
                     "length": tf.FixedLenFeature([], dtype=tf.int64)
@@ -72,24 +101,65 @@ def read_and_decode_sequence(filename_queue, config):
                     "skeleton": tf.FixedLenSequenceFeature([], dtype=tf.string),
                 })
 
-
-        # Fetch required data fields.
+        
+        # Fetch required data fields. 
         # TODO: Customize for your design. Assume that only the RGB images are used for now.
-        # Decode the serialized RGB images.
+        # Decode the serialized RGB images.    
         seq_rgb = tf.decode_raw(sequence_encoded['rgb'], tf.uint8)
+        seq_seg = tf.decode_raw(sequence_encoded['segmentation'], tf.uint8)
+        seq_dep = tf.decode_raw(sequence_encoded['depth'], tf.uint8)
+        
+        # Output dimensionality: [seq_len, height, width, numChannels]
+        
+        # tf.map_fn applies the preprocessing function to every image in the sequence, i.e., frame.
+        seq_rgb = tf.map_fn(lambda x: preprocessing_op(x, config),
+                                elems=(seq_rgb,seq_seg,seq_dep),
+                                dtype=tf.float32,
+                                back_prop=False)
+        
         seq_label = context_encoded['label']
-        # Tensorflow requires the labels start from 0. Before you create submission csv,
+        
+        # Tensorflow requires the labels start from 0. Before you create submission csv, 
         # increment the predictions by 1.
         seq_label = seq_label - 1
         seq_len = tf.to_int32(context_encoded['length'])
-        # Output dimnesionality: [seq_len, height, width, numChannels]
-        # tf.map_fn applies the preprocessing function on every image in the sequence, i.e., frame.
-        seq_rgb = tf.map_fn(lambda x: preprocessing_op(x, config),
-                                elems=seq_rgb,
-                                dtype=tf.float32,
-                                back_prop=False)
-
+        
+        """
+        # Use skeleton only.
+        seq_skeleton = tf.decode_raw(sequence_encoded['skeleton'], tf.float32)
+        # Normalize skeleton so that every pose is a unit length vector.
+        seq_skeleton = tf.nn.l2_normalize(seq_skeleton, dim=1)
+        seq_skeleton.set_shape([None, config['skeleton_size']])
+        
+        seq_len = tf.to_int32(context_encoded['length'])
+        seq_label = context_encoded['label']
+        # Tensorflow requires the labels start from 0. Before you create submission csv, 
+        # increment the predictions by 1.
+        seq_label = seq_label - 1
+        """
+        
         return [seq_rgb, seq_label, seq_len]
+    
+
+def input_pipeline(filenames, config, name='input_pipeline', shuffle=True):
+    with tf.name_scope(name):
+        # Create a queue of TFRecord input files.
+        filename_queue = tf.train.string_input_producer(filenames, num_epochs=config['num_epochs'], shuffle=shuffle)
+        # Read the data from TFRecord files, decode and create a list of data samples by using threads.
+        sample_list = [read_and_decode_sequence(filename_queue, config) for _ in range(config['ip_num_read_threads'])]
+        # Create batches.
+        # Since the data consists of variable-length sequences, allow padding by setting dynamic_pad parameter.
+        # "batch_join" creates batches of samples and pads the sequences w.r.t the max-length sequence in the batch.
+        # Hence, the padded sequence length can be different for different batches.
+        batch_rgb, batch_labels, batch_lens = tf.train.batch_join(sample_list,
+                                                    batch_size=config['batch_size'],
+                                                    capacity=config['ip_queue_capacity'],
+                                                    enqueue_many=False,
+                                                    dynamic_pad=True,
+                                                    allow_smaller_final_batch = False,
+                                                    name="batch_join_and_pad")
+
+        return batch_rgb, batch_labels, batch_lens
 
 def read_and_decode_sequence_test_data(filename_queue, config):
     """
@@ -132,13 +202,16 @@ def read_and_decode_sequence_test_data(filename_queue, config):
         # TODO: Customize for your design. Assume that only the RGB images are used for now.
         # Decode the serialized RGB images.
         seq_rgb = tf.decode_raw(sequence_encoded['rgb'], tf.uint8)
+        seq_seg = tf.decode_raw(sequence_encoded['segmentation'], tf.uint8)
+        seq_dep = tf.decode_raw(sequence_encoded['depth'], tf.uint8)
         seq_len = tf.to_int32(context_encoded['length'])
         # Output dimnesionality: [seq_len, height, width, numChannels]
         # tf.map_fn applies the preprocessing function on every image in the sequence, i.e., frame.
         seq_rgb = tf.map_fn(lambda x: preprocessing_op(x, config),
-                                elems=seq_rgb,
+                                elems=(seq_rgb,seq_seg,seq_dep),
                                 dtype=tf.float32,
                                 back_prop=False)
+        
         seq_id = context_encoded['id']
 
         return [seq_rgb, seq_id, seq_len]
